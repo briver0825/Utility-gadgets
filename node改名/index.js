5de97507817b8c00e603c567aea69bf7778fafa5
朱鑫 <3118446488@qq.com>
2021-11-27 00:53:43 +0800

    first push

---
+++ node改名/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require('path')
+
+//文件夹地址
+const handFilePath = path.resolve(`E:\\xxxxxxxxxxxxxxxxx`)
+
+// 需要过滤的文字水印
+const filterName = 'www.baidu.com'
+
+
+deepReadFiles(handFilePath)
+
+function deepReadFiles(filePath) {
+    fs.readdirSync(filePath).forEach(item => {
+        if (fs.statSync(path.resolve(`${filePath}\\${item}`)).isDirectory()) {
+            const dirPath = `${filePath}\\${item}`
+            deepReadFiles(dirPath)
+        } else {
+            handFile(`${filePath}\\${item}`)
+        }
+    })
+}
+
+function handFile(filePath) {
+    fs.renameSync(filePath, filePath.replace(filterName, ''))
+}
